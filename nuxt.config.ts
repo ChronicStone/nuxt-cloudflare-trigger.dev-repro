0711cbf8a68bcb0ed608daa079861008345759b1
ChronicStone <thao@agorastore.fr>
2024-12-23 15:44:01 +0100

    refactor(app): setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  // https://nuxt.com/modules
+  modules: [
+    '@nuxthub/core',
+    '@nuxt/eslint',
+  ],
+
+  // https://devtools.nuxt.com
+  devtools: { enabled: true },
+
+  // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
+  runtimeConfig: {
+    trigger: {
+      secretKey: ''
+    },
+    public: {
+      // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
+      helloText: 'Hello from the Edge 👋',
+    },
+  },
+  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: '2024-07-30',
+
+  // https://hub.nuxt.com/docs/getting-started/installation#options
+  hub: {},
+
+  // https://eslint.nuxt.com
+  eslint: {
+    config: {
+      stylistic: {
+        quotes: 'single',
+      },
+    },
+  },
 })
